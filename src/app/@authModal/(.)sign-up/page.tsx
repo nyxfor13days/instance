c7e0e58f79c91591627b324bff0f4ee4a2e2f547
Nyxfor13days <nyxfor13days@gmail.com>
2023-08-27 03:37:26 +0530

    Updated Backdrop for Intercepts

--- src/app/@authModal/(.)sign-up/page.tsx
+++ src/app/@authModal/(.)sign-up/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 export default function page() {
   return (
-    <div className="fixed inset-0 bg-background/25 z-10">
+    <div className="fixed inset-0 bg-background/50 z-10 backdrop-blur">
       <div className="container flex items-center h-full max-w-lg mx-auto">
         <div className="relative bg-popover text-popover-foreground w-full h-fit py-20 px-2 rounded shadow-md">
           <div className="absolute top-4 right-4">
