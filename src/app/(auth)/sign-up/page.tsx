55844dfdd241e74304657a19c601b33d130fe0af
Nyxfor13days <nyxfor13days@gmail.com>
2023-08-27 02:36:57 +0530

    Added Sign up Page

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/Button";
+import { cn } from "@/lib/utils";
+import SignIn from "@/components/SignIn";
+import { ArrowLeftIcon } from "@radix-ui/react-icons";
+import SignUp from "@/components/SignUp";
+
+export default function page() {
+  return (
+    <div className="absolute inset-0">
+      <div className="h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20">
+        <Link
+          href="/"
+          className={cn(
+            buttonVariants({ variant: "secondary", size: "icon" }),
+            "self-start -mt-20 flex items-center gap-2"
+          )}
+        >
+          <ArrowLeftIcon />
+        </Link>
+
+        <SignUp />
+      </div>
+    </div>
+  );
+}
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import UserAuthForm from "./UserAuthForm";
 import Link from "next/link";
 
-export default function SignIn() {
+export default function SignUp() {
   return (
     <div className="container mx-auto w-full flex flex-col justify-center space-y-6 sm:w-[400px]">
       <div className="flex flex-col space-y-2 text-center">
-        <h1 className="text-2xl font-semibold tracking-tight">Welcome Back</h1>
+        <h1 className="text-2xl font-semibold tracking-tight">Welcome to Instance</h1>
         <p className="text-sm max-w-xs mx-auto">
           By continuing you agree to our{" "}
           <Link href="#" className="text-accent-foreground/75">
@@ -22,9 +22,9 @@ export default function SignIn() {
         <UserAuthForm />
 
         <p className="text-sm max-w-xs mx-auto">
-          Don&apos;t have an account?{" "}
-          <Link href="/sign-up" className="text-accent-foreground/75 underline underline-offset-8">
-            Sign Up
+          Already have an account?{" "}
+          <Link href="/sign-in" className="text-accent-foreground/75 underline underline-offset-8">
+            Sign In
           </Link>
         </p>
       </div>
