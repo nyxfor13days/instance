776ed7c1c01cbb2464cc2590918d4813cb7fc86a
Nyxfor13days <nyxfor13days@gmail.com>
2023-08-27 23:36:59 +0530

    Update schema.ts

--- src/app/(user)/profile/[id]/create/schema.ts
+++ src/app/(user)/profile/[id]/create/schema.ts
@@ -22,85 +22,60 @@ export const educationSchema = z.object({
   description: z.string().min(1).max(500),
 });
 
+export const socialsSchema = z.object({
+  name: z.string().min(1).max(50),
+  url: z.string().min(1).max(50),
+});
+
+export const experienceSchema = z.object({
+  title: z.string().min(1).max(50),
+  company: z.string().min(1).max(50),
+  location: z.string().min(1).max(50),
+  from: z.string().min(1).max(50),
+  to: z.string().min(1).max(50),
+  current: z.boolean(),
+  description: z.string().min(1).max(500),
+});
+export const projectsSchema = z.object({
+  name: z.string().min(1).max(50),
+  description: z.string().min(1).max(500),
+  url: z.string().min(1).max(50),
+});
+export const achievementsSchema = z.object({
+  name: z.string().min(1).max(50),
+  description: z.string().min(1).max(500),
+});
+export const hobbiesSchema = z.object({
+  name: z.string().min(1).max(50),
+  description: z.string().min(1).max(500),
+});
+export const languagesSchema = z.object({
+  name: z.string().min(1).max(50),
+  level: z.number().min(1).max(5),
+});
+export const interestsSchema = z.object({
+  name: z.string().min(1).max(50),
+  description: z.string().min(1).max(500),
+});
+export const referencesSchema = z.object({
+  name: z.string().min(1).max(50),
+  description: z.string().min(1).max(500),
+});
+export const certificationsSchema = z.object({
+  name: z.string().min(1).max(50),
+  description: z.string().min(1).max(500),
+});
+
 export const arrayFormSchema = z.object({
   skills: z.array(skillSchema).optional(),
   education: z.array(educationSchema).optional(),
-  socials: z.array(
-    z
-      .object({
-        name: z.string().min(1).max(50),
-        url: z.string().min(1).max(50),
-      })
-      .optional()
-  ),
-  experience: z.array(
-    z
-      .object({
-        title: z.string().min(1).max(50),
-        company: z.string().min(1).max(50),
-        location: z.string().min(1).max(50),
-        from: z.string().min(1).max(50),
-        to: z.string().min(1).max(50),
-        current: z.boolean(),
-        description: z.string().min(1).max(500),
-      })
-      .optional()
-  ),
-  projects: z.array(
-    z
-      .object({
-        name: z.string().min(1).max(50),
-        description: z.string().min(1).max(500),
-        url: z.string().min(1).max(50),
-      })
-      .optional()
-  ),
-  achievements: z.array(
-    z
-      .object({
-        name: z.string().min(1).max(50),
-        description: z.string().min(1).max(500),
-      })
-      .optional()
-  ),
-  hobbies: z.array(
-    z
-      .object({
-        name: z.string().min(1).max(50),
-        description: z.string().min(1).max(500),
-      })
-      .optional()
-  ),
-  languages: z.array(
-    z
-      .object({
-        name: z.string().min(1).max(50),
-        level: z.number().min(1).max(5),
-      })
-      .optional()
-  ),
-  interests: z.array(
-    z
-      .object({
-        name: z.string().min(1).max(50),
-        description: z.string().min(1).max(500),
-      })
-      .optional()
-  ),
-  references: z.array(
-    z
-      .object({
-        name: z.string().min(1).max(50),
-        description: z.string().min(1).max(500),
-      })
-      .optional()
-  ),
-  certifications: z.array(
-    z
-      .object({
-        name: z.string().min(1).max(50),
-        description: z.string().min(1).max(500),
-      })
-      .optional()
-  ),
+  socials: z.array(socialsSchema).optional(),
+  experience: z.array(experienceSchema).optional(),
+  projects: z.array(projectsSchema).optional(),
+  achievements: z.array(achievementsSchema).optional(),
+  hobbies: z.array(hobbiesSchema).optional(),
+  languages: z.array(languagesSchema).optional(),
+  interests: z.array(interestsSchema).optional(),
+  references: z.array(referencesSchema).optional(),
+  certifications: z.array(certificationsSchema).optional(),
 });
